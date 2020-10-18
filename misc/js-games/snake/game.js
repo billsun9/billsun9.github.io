6560fe98affc3583e5733e109ad2740968274ce3
billsun9 <53660703+billsun9@users.noreply.github.com>
2020-10-18 11:55:12 -0400

    changed path

--- misc/js-games/snake/game.js
+++ misc/js-games/snake/game.js
@@ -8,7 +8,7 @@ let gameOver = false;
 function main(currentTime) {
     if (gameOver) {
         if (confirm('You lost. Press ok to restart.')) {
-            window.location = '/'; //refresh to restart
+            window.location = '/misc/js-games/snake/'; //refresh to restart (live path)
         }
         return
     }
